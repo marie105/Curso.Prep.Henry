4afd930d2fa2b8dcf46f681f8e1a85ec70f85dff
marie105 <lmarie.roots@gmail.com>
2022-03-09 21:10:36 -0500

    segundo commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -34,7 +34,7 @@ function conection(status) {
   //Devolver el estado de conexión de usuario en cada uno de los casos.
   if(status === 1)
   {
-    return "online";
+    return "Online";
   } 
   else if (status ===2)
   {
@@ -63,7 +63,7 @@ function saludo(idioma) {
   }
   else if (idioma === "ingles")
   {
-    return "Hello";
+    return "Hello!";
   }
   else
   {
@@ -88,7 +88,7 @@ function colors(color) {
   
  
     case "red":
-    return "this is red";
+    return "This is red";
     
 
     case "green":
@@ -161,17 +161,17 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if ( numero % 3 === 0)
+  if ( numero % 15 === 0)
+  {
+    return "fizzbuzz";
+  }
+  else if (numero % 3 === 0)
   {
     return "fizz";
   }
-  else if (numero % 5 === 0)
+  else if ( numero % 5 === 0)
   {
     return "buzz";
-  }
-  else if ( numero % 15 === 0)
-  {
-    return "fizzbuzz";
   }
   else
   {
@@ -187,22 +187,23 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > 0 && num1 > num2 && num1 > num3)
-  {
-    return "Número 1 es mayor y positivo";
-  }
-  else if (num1 < 0 || num2 < 0 || num3 < 0)
+  if (num1 < 0 || num2 < 0 || num3 < 0)
   {
     return "Hay negativos";
-  }
-  else if (num3 > num1 && num3 > num2)
-  {
-    return num3 + 1;
   }
   else if (num1 === 0 || num2 === 0 || num3 === 0)
   {
     return "Error";
   }
+  else if (num1 > 0 && num1 > num2 && num1 > num3)
+  {
+    return "Número 1 es mayor y positivo";
+  }
+  else if (num3 > num1 && num3 > num2)
+  {
+    return num3 + 1;
+  }
+  
   else 
   {
     return false;
@@ -215,24 +216,24 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if (numero === 2)
-  {
-    return true;
-  }
-  else if (numero < 2)
+  if (numero < 2)
   {
     return false;
   }
-  for (var i = 2; i < numero; i ++ )
+  else if (numero === 2)
   {
-    if ( numero % 1 === 0)
+    return true;
+  }
+  for ( var i = 2; i < numero; i ++ )
+  {
+    if (numero % i === 0)
     {
       return false;
     }
-    {
-      return true;
-    }
   }
+  return true;
+    
+
 }
 
 function esVerdadero(valor){
@@ -241,11 +242,11 @@ function esVerdadero(valor){
   //Escribe tu código aquí
   if ( valor === true)
   {
-    return "Soy verdadero"
+    return "Soy verdadero";
   }
   else ( valor === false)
   {
-    return "Soy falso"
+    return "Soy falso";
   }
 
 }
@@ -254,19 +255,42 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  var tablaDel6 = []
+  for (var i = 0; i < 11; i ++)
+  {
+    tablaDel6.push(6 * i)
+  }
+
+  {
+    return tablaDel6;
+  }
 }
+
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if ( numero  >  99  &&  numero  <  1000 )
+  {
+    return true;
+  }
+  return  false;
 }
+
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var a = numero;
+  var i = 0;
+  do
+  {
+    i = i + 1;
+    a = a + 5;
+  }
+  while(i < 8);
+  return a
 }
 
 
@@ -288,5 +312,5 @@ module.exports = {
   esVerdadero,
   tablaDelSeis,
   tieneTresDigitos,
-  hacermientras
+  doWhile
  } ;
